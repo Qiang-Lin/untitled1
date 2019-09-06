031ca2eed290a3322a090f10de821fb25ede5919
Qiang-Lin <qiang_lin1992@outlook.com>
2019-09-06 17:50:26 +0800

    Initial commit

--- CrawBasketballData.py
+++ CrawBasketballData.py
@@ -5,7 +5,6 @@ import pymongo
 class getNBAData():
     client = pymongo.MongoClient(host='localhost', port=27017)
     db = client['NBA']
-    collection1 = db['nba_player']
 
     def __init__(self, start_url='https://china.nba.com/static/data/player/stats_klay_thompson.json'):
         self.start_url = start_url
@@ -34,12 +33,30 @@ class getNBAData():
         df['atHome'] = team
         return df
 
+    # 把列表解析成数据框
+    def resol_dataLs1(self, dataLs, team, gameTime):
+        import pandas as pd
+        playerData = dataLs[team]['gamePlayers']
+        teamName = dataLs[team]['profile']['displayAbbr']
+        for i in range(len(playerData)):
+            playerData[i]['statTotal'].update(player=playerData[i]['profile']['displayName'])
+            playerData[i] = playerData[i]['statTotal']
+            playerData[i]['atHome'] = 'team'
+            playerData[i]['gameDesc']
+            self.storeData2MongoDB(self, playerData[i])
+        return ""
+
+    def storeData2MongoDB(self, data):
+        collection = self.db['nba_player']
+        collection.insert_one(data)
+        return None
+
     # 数据框直接写入mysql数据库，我的密码用*代替了
     def storeData2MySql(self, data, schema, table):
         import pymongo
         import pandas as pd
 
-        self.collection1.insert_one(data)
+        # self.collection1.insert_one(data)
         data.to_excel("D:\\NBA.xlsx")
 
         return None
@@ -56,12 +73,14 @@ class getNBAData():
         for each in game_id:
             r = requests.get(url % each)
             rawdata = json.loads(r.content)
-            homeTeam = self.resol_dataLs(rawdata['payload'], 'homeTeam')
-            awayTeam = self.resol_dataLs(rawdata['payload'], 'awayTeam')
             gameTime = datetime.datetime.fromtimestamp(int(rawdata['timestamp']) / 1000,
                                                        pytz.timezone('Asia/Shanghai')).strftime('%Y-%m-%d')
+            homeTeam = self.resol_dataLs1(rawdata['payload'], 'homeTeam', gameTime)
+            awayTeam = self.resol_dataLs1(rawdata['payload'], 'awayTeam', gameTime)
+
             tot_df = pd.concat([homeTeam, awayTeam])
             tot_df['gameDate'] = gameTime
+
             tot_df['gameDesc'] = '%s(%d)@%s(%d)' % (
                 tot_df[tot_df['atHome'] == 'awayTeam']['teamName'][0], rawdata['payload']['boxscore']['awayScore'],
                 tot_df[tot_df['atHome'] == 'homeTeam']['teamName'][0], rawdata['payload']['boxscore']['homeScore'])
