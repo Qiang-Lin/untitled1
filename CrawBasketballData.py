ca1c6140058bbaa73add70aeb3972fe779a1bc04
Qiang-Lin <qiang_lin1992@outlook.com>
2019-09-17 18:23:10 +0800

    daily match commit

--- CrawBasketballData.py
+++ CrawBasketballData.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 import pymongo
+from CrawDailyMatch import craw_daily_match
 
 
 class getNBAData():
     client = pymongo.MongoClient(host='localhost', port=27017)
     db = client['NBA']
+    collection1 = db['nba_player']
 
     def __init__(self, start_url='https://china.nba.com/static/data/player/stats_klay_thompson.json'):
         self.start_url = start_url
@@ -21,44 +23,31 @@ class getNBAData():
         return game_id
 
     # 把列表解析成数据框
-    def resol_dataLs(self, dataLs, team):
-        import pandas as pd
+    def resol_dataLs(self, dataLs, team, gameTime):
         playerData = dataLs[team]['gamePlayers']
         teamName = dataLs[team]['profile']['displayAbbr']
+        gameDesc = '%s(%d)@%s(%d)' % (
+            dataLs['homeTeam']['profile']['displayAbbr'], dataLs['boxscore']['awayScore'],
+            dataLs['awayTeam']['profile']['displayAbbr'], dataLs['boxscore']['homeScore'])
         for i in range(len(playerData)):
             playerData[i]['statTotal'].update(player=playerData[i]['profile']['displayName'])
             playerData[i] = playerData[i]['statTotal']
-        df = pd.DataFrame(playerData, columns=list(playerData[0].keys()))
-        df['teamName'] = teamName
-        df['atHome'] = team
-        return df
+            playerData[i]['atHome'] = team
+            playerData[i]['gameDesc'] = gameDesc
+        self.storeData2MongoDB(playerData)
+        return ""
 
-    # 把列表解析成数据框
-    def resol_dataLs1(self, dataLs, team, gameTime):
-        import pandas as pd
-        playerData = dataLs[team]['gamePlayers']
-        teamName = dataLs[team]['profile']['displayAbbr']
-        for i in range(len(playerData)):
-            playerData[i]['statTotal'].update(player=playerData[i]['profile']['displayName'])
-            playerData[i] = playerData[i]['statTotal']
-            playerData[i]['atHome'] = 'team'
-            playerData[i]['gameDesc']
-            self.storeData2MongoDB(self, playerData[i])
+    def resol_dataLs1(self):
         return ""
 
     def storeData2MongoDB(self, data):
-        collection = self.db['nba_player']
-        collection.insert_one(data)
+        self.collection1.insert_many(data)
         return None
 
-    # 数据框直接写入mysql数据库，我的密码用*代替了
-    def storeData2MySql(self, data, schema, table):
-        import pymongo
-        import pandas as pd
-
+    # 数据框直接写入excel
+    def storeData2Excel(self, data, schema, table):
         # self.collection1.insert_one(data)
         data.to_excel("D:\\NBA.xlsx")
-
         return None
 
     # 主函数
@@ -67,7 +56,7 @@ class getNBAData():
         import pytz
         import requests
         import json
-        import pandas as pd
+        count = 0
         game_id = self.get_gameid(self.start_url)
         url = 'https://china.nba.com/static/data/game/snapshot_%s.json'
         for each in game_id:
@@ -75,16 +64,18 @@ class getNBAData():
             rawdata = json.loads(r.content)
             gameTime = datetime.datetime.fromtimestamp(int(rawdata['timestamp']) / 1000,
                                                        pytz.timezone('Asia/Shanghai')).strftime('%Y-%m-%d')
-            homeTeam = self.resol_dataLs1(rawdata['payload'], 'homeTeam', gameTime)
-            awayTeam = self.resol_dataLs1(rawdata['payload'], 'awayTeam', gameTime)
-
-            tot_df = pd.concat([homeTeam, awayTeam])
-            tot_df['gameDate'] = gameTime
-
-            tot_df['gameDesc'] = '%s(%d)@%s(%d)' % (
-                tot_df[tot_df['atHome'] == 'awayTeam']['teamName'][0], rawdata['payload']['boxscore']['awayScore'],
-                tot_df[tot_df['atHome'] == 'homeTeam']['teamName'][0], rawdata['payload']['boxscore']['homeScore'])
-            self.storeData2MySql(tot_df, 'python_spyder', 'worriorsGame')
+            homeTeamData = self.resol_dataLs(rawdata['payload'], 'homeTeam', gameTime)
+            awayTeamData = self.resol_dataLs(rawdata['payload'], 'awayTeam', gameTime)
+            count = count + 1
+            print('\r当前进度:{:.2f}%'.format(count * 100 / len(game_id)), end='')
+            # homeTeamName = rawdata['payload']['homeTeam']['profile']['displayAbbr']
+            # awayTeamName = rawdata['payload']['homeTeam']['profile']['displayAbbr']
+            # tot_df = pd.concat([homeTeam, awayTeam])
+            # tot_df['gameDate'] = gameTime
+            # tot_df['gameDesc'] = '%s(%d)@%s(%d)' % (
+            #     tot_df[tot_df['atHome'] == 'awayTeam']['teamName'][0], rawdata['payload']['boxscore']['awayScore'],
+            #     tot_df[tot_df['atHome'] == ' homeTeam']['teamName'][0], rawdata['payload']['boxscore']['homeScore'])
+            # self.storeData2MySql(tot_df, 'python_spyder', 'worriorsGame')
 
 
 if __name__ == '__main__':
